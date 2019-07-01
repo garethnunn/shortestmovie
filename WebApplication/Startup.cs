99014bad976f1a8d046b04f7bde7bbf1b91aa43e
garethnunn <garethnunn@gmail.com>
2019-07-01 17:19:32 +0100

    deployment test

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -33,7 +33,7 @@ namespace WebApplication
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+             
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<WebApplicationContext>(options =>
