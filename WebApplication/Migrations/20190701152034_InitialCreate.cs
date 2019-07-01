3dabaaee501336b2bc5f507fccd9b1cde53a0cce
garethnunn <garethnunn@gmail.com>
2019-07-01 16:55:08 +0100

    MYSQL Migration

---
+++ WebApplication/Migrations/20190701152034_InitialCreate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+        }
+    }
+}
---
+++ WebApplication/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+
+        public string Rating { get; set; }
+
+    }
+}
