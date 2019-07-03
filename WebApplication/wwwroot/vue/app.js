5da642f3ae11d764d4a093ad9c7c555502ac411c
garethnunn <garethnunn@gmail.com>
2019-07-04 00:23:49 +0100

    Added poster pullthrough

--- WebApplication/wwwroot/vue/app.js
+++ WebApplication/wwwroot/vue/app.js
@@ -1,7 +1,7 @@
 ﻿
 Vue.component('searchresult', {
     props: ['result'],
-    template: '<div class="searchresults" >{{ result.title }} {{ result.runtime }} minutes</div>'
+    template: '<div class="searchresults" >{{ result.title }} {{ result.runtime }} minutes <img v-bind:src="\'http://image.tmdb.org/t/p/w185/\' + result.images.posters[0].file_path" /></div>'
 })
 
 var app7 = new Vue({
@@ -19,11 +19,12 @@ var app7 = new Vue({
         
             axios.get("https://api.themoviedb.org/3/search/movie?api_key=d6f12ddc103a301a347006b8083d3626&query=" + this.searchQuery)
                 .then(response => {
-                    this.searchResults = response.data.results.slice(0, 1);
+                    this.searchResults = response.data.results.slice(0, 3);
                     var promises = [];
 
                     this.searchResults.forEach(function (e) {                    
-                        promises.push(axios.get("https://api.themoviedb.org/3/movie/" + e.id + "?api_key=d6f12ddc103a301a347006b8083d3626"));
+                        promises.push(axios.get("https://api.themoviedb.org/3/movie/" + e.id + "?api_key=d6f12ddc103a301a347006b8083d3626&append_to_response=images"));
+
                     });
 
                     axios.all(promises).then(function (results) {
